964a39350106f68d7e6241ca6e6b2b38dc3c7d01
LiuMY <liumingyi@zhinanmao.com>
2016-12-15 11:36:22 +0800

    es6

--- bin/development.js
+++ bin/development.js
@@ -1,80 +1,95 @@
-#!/usr/bin/env node
-process.env.NODE_ENV = 'development'
-console.log('Waiting for webpacking ...')
-require('babel-polyfill')
+import http from 'http';
+import Koa from 'koa';
+import KWM from 'koa-webpack-middleware';
+import path from 'path';
+import webpack from 'webpack';
+import chokidar from 'chokidar';
+import 'babel-polyfill';
+import debugFunc from 'debug';
+import middlewareRegister from '../platforms/server/middlewareRegister';
+import webpackConfig from '../config/webpack.development';
+import config from '../platforms/common/config';
+
+const debug = debugFunc('app:dev:server');
+
+const app = new Koa();
+const devMiddleware = KWM.devMiddleware;
+const hotMiddleware = KWM.hotMiddleware;
+const compiler = webpack(webpackConfig);
+
+debug('Waiting for webpack ...');
+
 require('babel-core/register')({
   plugins: [
     ['babel-plugin-transform-require-ignore', {
-      extensions: ['.less', '.css']
+      extensions: ['.less', '.css'],
     }],
     ['inline-replace-variables', {
-      __SERVER__: true
-    }]
-  ]
-})
+      __SERVER__: true,
+    }],
+  ],
+});
+
 require('asset-require-hook')({
   extensions: ['jpg', 'jpeg', 'png', 'gif', 'svg', 'tif', 'tiff', 'webp'],
   name: '/build/[name].[ext]',
-  limit: 10000
-})
+  limit: 10000,
+});
 
-var Koa = require('koa')
-var app = new Koa()
-var path = require('path')
-var middlewareRegister = require('../platforms/server/middlewareRegister')
-var webpack = require('webpack')
-var KWM = require('koa-webpack-middleware')
-var devMiddleware = KWM.devMiddleware
-var hotMiddleware = KWM.hotMiddleware
-var chokidar = require('chokidar')
-var webpackConfig = require('../webpack.development')
-var compiler = webpack(webpackConfig)
-var config = require('../platforms/common/config')
-var devMiddlewareInstance = devMiddleware(compiler, {
+const devMiddlewareInstance = devMiddleware(compiler, {
   noInfo: true,
   watchOptions: {
     aggregateTimeout: 300,
-    poll: false
+    poll: false,
   },
   publicPath: '/build/',
   stats: {
-    colors: true
-  }
-})
-var hotMiddlewareInstance = hotMiddleware(compiler, {
-  log: console.log,
-  path: '/__webpack_hmr',
-  heartbeat: 10 * 1000
-})
+    colors: true,
+  },
+});
 
-app.env = 'development'
-app.use(devMiddlewareInstance)
-app.use(hotMiddlewareInstance)
-middlewareRegister(app) // reg middleware
+const hotMiddlewareInstance = hotMiddleware(compiler, {
+  log: debug,
+  path: '/__webpack_hmr',
+  heartbeat: 10 * 1000,
+});
+
+app.env = 'development';
+app.use(devMiddlewareInstance);
+app.use(hotMiddlewareInstance);
+middlewareRegister(app); // reg middleware
 // error logger
-app.on('error', function (err, ctx) {
-  console.log('error occured:', err.stack)
-})
+app.on('error', (err) => {
+  debug('error occured:', err.stack);
+});
 
 // listen
-var server = require('http').createServer(app.callback())
-var watcher = chokidar.watch([
+const server = http.createServer(app.callback());
+const watcher = chokidar.watch([
   path.join(__dirname, '../app'),
-  path.join(__dirname, '../platforms')
-])
-watcher.on('ready', function () {
-  watcher.on('all', function (e, p) {
-    console.log("Clearing module cache");
-    Object.keys(require.cache).forEach(function(id) {
-      if (/[\/\\](app|platforms)[\/\\]/.test(id)) delete require.cache[id];
+  path.join(__dirname, '../platforms'),
+]);
+
+watcher.on('ready', () => {
+  watcher.on('all', () => {
+    debug('Clearing module cache');
+    Object.keys(require.cache).forEach((id) => {
+      if (/[/\\](app|platforms)[/\\]/.test(id)) delete require.cache[id];
     });
-  })
-})
-var isListened = false
-compiler._plugins['after-compile'].push(function (compilation, callback) {
-  callback()
-  !isListened && server.listen(config.port, function () {
-    console.log('App started, at port %d, CTRL + C to terminate', config.port)
-    isListened = true
-  })
-})
+  });
+});
+
+let isListened = false;
+compiler._plugins['after-compile'].push((compilation, callback) => {
+  callback();
+  if (!isListened) {
+    server.listen(config.port, (err) => {
+      if (err) {
+        debug(err);
+        return;
+      }
+      debug('App started, at port %d, CTRL + C to terminate', config.port);
+      isListened = true;
+    });
+  }
+});
